c7b60958a553304669f5bce8f6738d9cfdab0d09
Kirill <sobolevkirill0@gmail.com>
2025-11-13 17:18:34 +0300

    Initial commit

---
+++ src/main/java/com/sobolev/spring/filemanageruniversity/FileManagerUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.sobolev.spring.filemanageruniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobolev/spring/filemanageruniversity/FileManagerUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobolev.spring.filemanageruniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
